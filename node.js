938c4db50a0d6ce3d5a0c7a050cec80242750038
pranavsram <48902428+pranavsram@users.noreply.github.com>
2020-02-12 09:20:53 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,1 +1,2 @@
-/ PRANAV
+a=[person1={name:"pranav",clas:"11",age:"20",display:function(){console.log(this.name+this.age+this.clas)}},person2={name:"ravi",clas:"11",age:"20",display:function(){console.log(this.name+this.age+this.clas)}},person3={name:"ravi",clas:"11",age:"20",display:function(){console.log(this.name+this.age+this.clas)}},person4={name:"ravi",clas:"11",age:"20",display:function(){console.log(this.name+this.age+this.clas)}},person5={name:"ravi",clas:"11",age:"20",display:function(){console.log(this.name+this.age+this.clas)}}]
+for(i=0;i<a.length;i++){v.push(a[i].name);ag.push(a[i].age);cl.push(a[i].clas)}
